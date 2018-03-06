79a44eb81e8550fc897767536dead44548b475a5
Steven-Jia <stevenjia@outlook.com>
2018-03-07 02:55:55 +0800

    Fix typo

--- src/main/java/seedu/address/model/group/exceptions/DuplicateGroupException.java
+++ src/main/java/seedu/address/model/group/exceptions/DuplicateGroupException.java
@@ -7,6 +7,6 @@ import seedu.address.commons.exceptions.DuplicateDataException;
  */
 public class DuplicateGroupException extends DuplicateDataException {
     public DuplicateGroupException() {
-        super("Operation would result in duplicate gruops");
+        super("Operation would result in duplicate groups");
     }
 }
